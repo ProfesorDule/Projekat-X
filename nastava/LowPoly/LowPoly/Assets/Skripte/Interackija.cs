3819fa201f8f0822483b82d74c17970dc0363934
ProfesorDule <dusanz@bobaz.net>
2017-03-04 12:48:51 +0100

    Ubacena interakcija sa NPC-om

---
+++ nastava/LowPoly/LowPoly/Assets/Skripte/Interackija.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interackija : MonoBehaviour {
+
+    public UnityEngine.AI.NavMeshAgent playerAgent;
+
+    public virtual void PomerajDoInterakcije(UnityEngine.AI.NavMeshAgent playerAgent)
+    {
+        this.playerAgent = playerAgent;
+        playerAgent.destination = this.transform.position;
+
+        Interakt();
+    }
+
+    public virtual void Interakt()
+    {
+        Debug.Log("Interakcija sa baznom klasom");
+    }
+
+}

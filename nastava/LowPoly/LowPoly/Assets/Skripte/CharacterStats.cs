ef21bcd61af2fc0e1fda2779d9fd848d5b9b8dba
ProfesorDule <dusanz@bobaz.net>
2017-03-18 12:39:18 +0100

    Ubacen sistem za statove

---
+++ nastava/LowPoly/LowPoly/Assets/Skripte/CharacterStats.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterStats : MonoBehaviour {
+
+    public List<BazniStatovi> stat = new List<BazniStatovi>();
+
+	void Start () {
+        stat.Add(new BazniStatovi(4, "Snaga", "Nivo snage"));
+        stat[0].DodajBonus(new StatBonus(5));
+        Debug.Log("Snaga: " + stat[0].GetStat());
+    }
+	
+}

ec80d994e720b66d14cd0ab65ebe4fd738c0af29
ProfesorDule <dusanz@bobaz.net>
2017-04-01 12:12:22 +0200

    Nastanak Funa?

---
+++ nastava/LowPoly/Fun/Assets/moveBall.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveBall : MonoBehaviour {
+
+    private GameObject ball;
+    private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        ball = GameObject.Find("gameBall");
+        rb = ball.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float turn = Input.GetAxis("Horizontal");
+        rb.AddTorque(transform.up * 1000 * turn);
+	}
+}
